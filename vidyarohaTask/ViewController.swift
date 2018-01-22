7c81e1809c270ea3f09240da46192658399db3bd
Sai <sai@Sais-Mac.local>
2018-01-22 14:08:37 +0530

    Initial Commit

---
+++ vidyarohaTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vidyarohaTask
+//
+//  Created by Sai on 22/01/18.
+//  Copyright © 2018 Sai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
